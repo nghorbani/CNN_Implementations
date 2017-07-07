048f3b2c3356eb94ff06cb5057680841de6e92b0
Nima Ghorbani <nima.gbani@gmail.com>
2017-07-07 21:03:02 +0200

    a fix for batch norm

--- Goodfellow_GenerativeAdversarialNetworks/tools_networks.py
+++ Goodfellow_GenerativeAdversarialNetworks/tools_networks.py
@@ -13,7 +13,8 @@ def deconv(X, is_training, kernel_w, Cout, name, stride=1, act='ReLu', useBN=Fal
     with tf.device('/gpu:0'):
         Y = tf.nn.bias_add(tf.nn.conv2d_transpose(X, W, strides=[1, stride, stride, 1], output_shape=output_shape, padding='VALID'), b)
         if useBN == True:
-            Y = batch_norm(Y, is_training, trainable,name='%s_BN' % name)
+            # Y = batch_norm(Y, is_training, trainable,name='%s_BN' % name)
+            Y = batch_norm_wrapper(Y, is_training, trainable, name='%s_BN' % name)
         if act != None:
             if act.lower() == 'relu':
                 Y = tf.nn.relu(Y)
@@ -38,7 +39,8 @@ def conv(X, is_training, kernel_w, Cout, name, stride=1, act='ReLu', useBN=False
     with tf.device('/gpu:0'):
         Y = tf.nn.bias_add(tf.nn.conv2d(X, W, strides=[1, stride, stride, 1], padding='VALID'), b)
         if useBN == True:
-            Y = batch_norm(Y, is_training,trainable, name='%s_BN' % name)
+            # Y = batch_norm(Y, is_training,trainable, name='%s_BN' % name)
+            Y = batch_norm_wrapper(Y, is_training, trainable, name='%s_BN' % name)
         if act != None:
             if act.lower() == 'relu':
                 Y = tf.nn.relu(Y)
@@ -66,7 +68,8 @@ def dense(X, is_training, Dout, name, trainable=True, act='ReLu', useBN=False, s
         Y = tf.nn.bias_add(tf.matmul(X, W), b)
         
         if useBN == True:
-            Y = batch_norm(Y, is_training, trainable,name='%s_BN' % name)
+            # Y = batch_norm(Y, is_training, trainable,name='%s_BN' % name)
+            Y = batch_norm_wrapper(Y, is_training, trainable, name='%s_BN' % name)
    
         if act != None:
             if act.lower() == 'relu':
@@ -85,50 +88,84 @@ def dense(X, is_training, Dout, name, trainable=True, act='ReLu', useBN=False, s
         tf.summary.histogram('%s_bias_histogram' % (name), b)    
     return Y 
 
-def batch_norm(X, is_training,trainable, name='BN', eps=1e-8):
-#     Y = tf.contrib.layers.batch_norm(inputs=X,
-#                                    decay=.999,
-#                                    center=True,
-#                                    scale=True,
-#                                    updates_collections=None,
-#                                    is_training=is_training,
-#                                    scope=name,
-#                                    fused=False,
-#                                    trainable=trainable)
-
-    if X.get_shape().ndims == 4:
-        batch_mean, batch_var = tf.nn.moments(X, [0, 1, 2])
-        Y = (X-batch_mean) / tf.sqrt(batch_var+eps)
+# def batch_norm(X, is_training,trainable, name='BN', eps=1e-8):
+#     with tf.device('/gpu:0'):
+#     #     Y = tf.contrib.layers.batch_norm(inputs=X,
+#     #                                    decay=.999,
+#     #                                    center=True,
+#     #                                    scale=True,
+#     #                                    updates_collections=None,
+#     #                                    is_training=is_training,
+#     #                                    scope=name,
+#     #                                    fused=False,
+#     #                                    trainable=trainable)
+#     
+#         if X.get_shape().ndims == 4:
+#             batch_mean, batch_var = tf.nn.moments(X, [0, 1, 2])
+#             Y = (X-batch_mean) / tf.sqrt(batch_var+eps)
+#      
+#         elif X.get_shape().ndims == 2:
+#             batch_mean, batch_var = tf.nn.moments(X, [0])
+#             Y = (X-batch_mean) / tf.sqrt(batch_var+eps)
+#     
+#         return Y
+    
+def batch_norm_wrapper(inputs, is_training, trainable, name, decay=0.999, epsilon=1e-5):
+    '''
+    correct way to use:
+    Y = Activation(BN(conv(W,X)+bias))
+    BN(Xbatch) = gamma((Xbatch-Meanbatch)/sqrt(Varbatch+epsilon)) + beta
+    '''
+    
+    beta = tf.get_variable(name='%s_beta' % name, shape=inputs.get_shape()[-1], trainable=trainable, dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+    scale = tf.get_variable(name='%s_scale' % name, shape=inputs.get_shape()[-1], trainable=trainable, dtype=tf.float32, initializer=tf.constant_initializer(1.0))  # scale each channel
+    
+    pop_mean = tf.get_variable(name='%s_pop_mean' % name, shape=inputs.get_shape()[-1], trainable=False, dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+    pop_var = tf.get_variable(name='%s_pop_var' % name, shape=inputs.get_shape()[-1], trainable=False, dtype=tf.float32, initializer=tf.constant_initializer(1.0))
  
-    elif X.get_shape().ndims == 2:
-        batch_mean, batch_var = tf.nn.moments(X, [0])
-        Y = (X-batch_mean) / tf.sqrt(batch_var+eps)
+    def BN_Train():
+        if inputs.get_shape().ndims == 4:
+            batch_mean, batch_var = tf.nn.moments(inputs, [0, 1, 2], name='moments')
+        elif inputs.get_shape().ndims == 2:
+            batch_mean, batch_var = tf.nn.moments(inputs, [0], name='moments')
 
-    return Y
+        train_mean = tf.assign(pop_mean,
+                               pop_mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(pop_var,
+                              pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(inputs, batch_mean, batch_var, beta, scale, epsilon)
+         
+    def BN_Test():
+        return tf.nn.batch_normalization(inputs, pop_mean, pop_var, beta, scale, epsilon)
+    
+    return tf.cond(is_training, lambda: BN_Train(), lambda: BN_Test())
 
 def dropout(Xin, is_training, p=0.8):
     with tf.device('/gpu:0'):
         Xout = tf.cond(is_training, lambda: tf.nn.dropout(Xin, keep_prob=p), lambda: tf.identity(Xin))
-    return Xout
+        return Xout
 
 def lrelu(X, leak=0.2):
-    Y = tf.maximum(X, leak * X)
-    return Y
+    with tf.device('/gpu:0'):
+        Y = tf.maximum(X, leak * X)
+        return Y
 
 def clipped_crossentropy(X, L):
-    Y = tf.clip_by_value(X, 1e-7, 1. - 1e-7)
-    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Y, labels=L))
+    with tf.device('/gpu:0'):
+        Y = tf.clip_by_value(X, 1e-7, 1. - 1e-7)
+        return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Y, labels=L))
 
 def bias_variable(shape, name=None, trainable=True):
     """return an initialized bias variable of a given shape"""
     with tf.device('/gpu:0'):
-        return tf.get_variable(name=name, shape=shape, dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+        return tf.get_variable(name=name, shape=shape, dtype=tf.float32, trainable=trainable, initializer=tf.constant_initializer(0.0))
     
 def weight_variable(shape, name=None, trainable=True):
     """return an initialized weight variable of a given shape
     shape: HxWxCinxCout
     """
     with tf.device('/gpu:0'):
-        #return tf.get_variable(name=name, shape=shape, dtype=tf.float32, initializer=xavier)
-        return tf.get_variable(name=name, shape=shape, dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+        # return tf.get_variable(name=name, shape=shape, dtype=tf.float32, initializer=xavier)
+        return tf.get_variable(name=name, shape=shape, dtype=tf.float32, trainable=trainable, initializer=tf.contrib.layers.xavier_initializer())
     
