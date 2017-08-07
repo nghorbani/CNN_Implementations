0c867639f29799368885fe332529f41129734275
Nima Ghorbani <nima.gbani@gmail.com>
2017-08-07 10:49:01 +0200

    directly following the paper

--- GenerativeAdversarialNetworks/WGAN.py
+++ GenerativeAdversarialNetworks/WGAN.py
@@ -6,123 +6,115 @@ This work is absolutely not an effort to reproduce exact results of the cited pa
 I have tried to implement my own limited understanding of the original paper in hope to get a better insight into their work. 
 Use this code with no warranty and please respect the accompanying license.
 '''
+from datetime import datetime
 
-import sys
+import sys, os
 sys.path.append('../common')
 
-from tools_config import data_dir, expr_dir
-import os
-import matplotlib.pyplot as plt
-from tools_train import get_train_params, OneHot, vis_square
-from datetime import datetime
 from tools_general import tf, np
-from tools_networks import deconv, conv, dense, clipped_crossentropy, dropout
+from tools_config import data_dir, expr_dir
+from tools_train import vis_square
+from tools_networks import deconv, conv, dense
 
-def concat_labels(X, labels):
-    if X.get_shape().ndims == 4:
-        X_shape = tf.shape(X)
-        labels_reshaped = tf.reshape(labels, [-1, 1, 1, 10])
-        a = tf.ones([X_shape[0], X_shape[1], X_shape[2], 10])
-        X = tf.concat([X, labels_reshaped * a], axis=3)
-    return X
-     
-def create_gan_G(z, labels, is_training, Cout=1, trainable=True, reuse=False, networktype='ganG'):
-    '''input : batchsize * 100 and labels to make the generator conditional
-        output: batchsize * 28 * 28 * 1'''
+from tensorflow.examples.tutorials.mnist import input_data
+  
+def create_gan_G(z, is_training, Cout=1, trainable=True, reuse=False, networktype='ganG'):
+    '''input : batchsize * latentDim
+       output: batchsize * 28 * 28 * 1'''
     with tf.variable_scope(networktype, reuse=reuse):
-        z = tf.concat(axis=-1, values=[z, labels])
-        Gout_op = dense(z, is_training, Cout=4 * 4 * 256, act='reLu', norm='batchnorm', name='dense1')
-        Gout_op = tf.reshape(Gout_op, shape=[-1, 4, 4, 256])  # 4
-        Gout_op = deconv(Gout_op, is_training, kernel_w=5, stride=2, Cout=256, trainable=trainable, act='reLu', norm='batchnorm', name='deconv1')  # 11
-        Gout_op = deconv(Gout_op, is_training, kernel_w=5, stride=2, Cout=128, trainable=trainable, act='reLu', norm='batchnorm', name='deconv2')  # 25
-        Gout_op = deconv(Gout_op, is_training, kernel_w=4, stride=Cout, Cout=1, act=None, norm=None, name='deconv3')  # 28
-        Gout_op = tf.nn.sigmoid(Gout_op)
-    return Gout_op
+        Gout = dense(z, is_training, Cout=7 * 7 * 256, trainable=trainable, act='reLu', norm='batchnorm', name='dense1')
+        Gout = tf.reshape(Gout, shape=[-1, 7, 7, 256])  # 7
+        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=128, trainable=trainable, act='reLu', norm='batchnorm', name='deconv1')  # 14
+        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=Cout, trainable=trainable, act=None, norm=None, name='deconv2')  # 28
+        Gout = tf.nn.sigmoid(Gout)
+    return Gout
 
-def create_gan_D(xz, labels, is_training, trainable=True, reuse=False, networktype='ganD'):
+def create_gan_D(xz, is_training, trainable=True, reuse=False, networktype='ganD'):
     with tf.variable_scope(networktype, reuse=reuse):
-        xz = concat_labels(xz, labels)
-        Dxz = conv(xz, is_training, kernel_w=5, stride=2, Cout=128, trainable=trainable, act='lrelu', norm=None, name='conv1')  # 12
-        Dxz = conv(Dxz, is_training, kernel_w=5, stride=2, Cout=256, trainable=trainable, act='lrelu', norm='batchnorm', name='conv2')  # 4
-        Dxz = conv(Dxz, is_training, kernel_w=2, stride=2, Cout=256, trainable=trainable, act='lrelu', norm='batchnorm', name='conv3')  # 2
-        Dxz = conv(Dxz, is_training, kernel_w=2, stride=2, Cout=1, trainable=trainable, act='lrelu', norm='batchnorm', name='conv4')  # 2
-        Dxz = tf.nn.sigmoid(Dxz)
-    return Dxz
+        Dout = conv(xz, is_training, kernel_w=4, stride=2, pad=1, Cout=128, trainable=trainable, act='lrelu', norm=None, name='conv1')  # 14
+        Dout = conv(Dout, is_training, kernel_w=4, stride=2, pad=1, Cout=256  , trainable=trainable, act='lrelu', norm='batchnorm', name='conv2')  # 7
+        Dout = conv(Dout, is_training, kernel_w=3, stride=1, pad=None, Cout=1, trainable=trainable, act=None, norm='batchnorm', name='conv4')  # 5
+        Dout = tf.nn.sigmoid(Dout)
+    return Dout
 
-def create_dcgan_trainer(base_lr=1e-4, networktype='dcgan'):
-    '''Train a Generative Adversarial Network'''
+def create_dcgan_trainer(base_lr=1e-4, networktype='dcgan', latentDim=100):
+    '''Train a Wasserstein Generative Adversarial Network'''
 
     is_training = tf.placeholder(tf.bool, [], 'is_training')
 
-    Zph = tf.placeholder(tf.float32, [None, 100])
-    Lph = tf.placeholder(tf.float32, [None, 10])  # we want to condition the generated out put on some parameters of the input
+    Zph = tf.placeholder(tf.float32, [None, latentDim])
     Xph = tf.placeholder(tf.float32, [None, 28, 28, 1])
 
-    Gout_op = create_gan_G(Zph, Lph, is_training, Cout=1, trainable=True, reuse=False, networktype=networktype + '_G') 
+    Gout_op = create_gan_G(Zph, is_training, Cout=1, trainable=True, reuse=False, networktype=networktype + '_G') 
 
-    fakeLogits_op = create_gan_D(Gout_op, Lph, is_training, trainable=True, reuse=False, networktype=networktype + '_D')
-    realLogits_op = create_gan_D(Xph, Lph, is_training, trainable=True, reuse=True, networktype=networktype + '_D')
+    fakeLogits = create_gan_D(Gout_op, is_training, trainable=True, reuse=False, networktype=networktype + '_D')
+    realLogits = create_gan_D(Xph, is_training, trainable=True, reuse=True, networktype=networktype + '_D')
     
-    ganG_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=networktype + '_G')
-    print(len(ganG_var_list), [var.name for var in ganG_var_list])
+    G_varlist = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=networktype + '_G')
+    print(len(G_varlist), [var.name for var in G_varlist])
 
-    ganD_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=networktype + '_D')
-    print(len(ganD_var_list), [var.name for var in ganD_var_list])
+    D_varlist = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=networktype + '_D')
+    print(len(D_varlist), [var.name for var in D_varlist])
 
-    Dscore = tf.reduce_mean(realLogits_op - fakeLogits_op)
-    Gscore = tf.reduce_mean(fakeLogits_op)
+    Dloss = tf.reduce_mean(realLogits) - tf.reduce_mean(fakeLogits)
+    Gloss = tf.reduce_mean(tf.abs(fakeLogits))
     
-    D_weights = [var for var in ganD_var_list if '_W' in var.name]
+    D_weights = [var for var in D_varlist if '_W' in var.name]
     D_weights_clip_op = [var.assign(tf.clip_by_value(var, -0.01, 0.01)) for var in D_weights]
                 
-    #Gtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Gscore, var_list=ganG_var_list)
-    #Dtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Dscore, var_list=ganD_var_list)
+    # Gtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Gloss, var_list=G_varlist)
+    # Dtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Dloss, var_list=D_varlist)
     
-    Gtrain_op = tf.train.RMSPropOptimizer(learning_rate=base_lr, decay=0.9).minimize(Gscore, var_list=ganG_var_list)
-    Dtrain_op = tf.train.RMSPropOptimizer(learning_rate=base_lr, decay=0.9).minimize(Dscore, var_list=ganD_var_list)
+    Gtrain_op = tf.train.RMSPropOptimizer(learning_rate=base_lr, decay=0.9).minimize(Gloss, var_list=G_varlist)
+    Dtrain_op = tf.train.RMSPropOptimizer(learning_rate=base_lr, decay=0.9).minimize(Dloss, var_list=D_varlist)
 
-    return Gtrain_op, Dtrain_op, D_weights_clip_op, Gscore, Dscore, is_training, Zph, Xph, Lph, Gout_op
+    return Gtrain_op, Dtrain_op, D_weights_clip_op, Gloss, Dloss, is_training, Zph, Xph, Gout_op
 
 if __name__ == '__main__':
     networktype = 'WGAN_MNIST'
     
     batch_size = 128
-    base_lr = 5e-5  # 1e-4
-    epochs = 300
+    base_lr = 5e-5  
+    epochs = 1000
+    latentDim = 100
     
     work_dir = expr_dir + '%s/%s/' % (networktype, datetime.strftime(datetime.today(), '%Y%m%d'))
     if not os.path.exists(work_dir): os.makedirs(work_dir)
-    
-    data, max_iter, test_iter, test_int, disp_int = get_train_params(data_dir + '/' + networktype, batch_size, epochs=epochs, test_in_each_epoch=1, networktype=networktype)
+        
+    data = input_data.read_data_sets(data_dir + '/' + networktype, reshape=False)
+    disp_int = 2 * int(data.train.num_examples / batch_size) #every two epochs
     
     tf.reset_default_graph() 
     sess = tf.InteractiveSession()
     
-    Gtrain_op, Dtrain_op, D_weights_clip_op, Gscore, Dscore, is_training, Zph, Xph, Lph, Gout_op = create_dcgan_trainer(base_lr, networktype=networktype)
+    Gtrain_op, Dtrain_op, D_weights_clip_op, Gloss, Dloss, is_training, Zph, Xph, Gout_op = create_dcgan_trainer(base_lr, networktype, latentDim)
     tf.global_variables_initializer().run()
     
     var_list = [var for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if (networktype.lower() in var.name.lower()) and ('adam' not in var.name.lower())]  
-    saver = tf.train.Saver(var_list=var_list, max_to_keep=1000)
+    saver = tf.train.Saver(var_list=var_list, max_to_keep=int(epochs * 0.1))
     # saver.restore(sess, expr_dir + 'ganMNIST/20170707/214_model.ckpt')  
-    
-    Z_test = np.random.uniform(size=[batch_size, 100], low=-1., high=1.).astype(np.float32)
-    labels_test = OneHot(np.random.randint(10, size=[batch_size]), n=10)    
-    
-    k = 5
-     
-    for it in range(1, max_iter): 
-        Z = np.random.uniform(size=[batch_size, 100], low=-1., high=1.).astype(np.float32)
-        X, labels = data.train.next_batch(batch_size)
-         
+        
+    it = 0       
+    while data.train.epochs_completed < epochs:
+        k = 100 if it < 25 or it % 500 == 0 else 5  # from the original pytorch implementation
+        dtemploss = 0
         for itD in range(k):
-            cur_Dscore, _ = sess.run([Dscore, Dtrain_op], feed_dict={Xph:X, Zph:Z, Lph:labels, is_training:True})
+            it += 1
+            Z = np.random.uniform(size=[batch_size, latentDim], low=-1., high=1.).astype(np.float32)
+            X, _ = data.train.next_batch(batch_size)
+        
+            cur_Dloss, _ = sess.run([Dloss, Dtrain_op], feed_dict={Xph:X, Zph:Z, is_training:True})
             sess.run(D_weights_clip_op)
+            dtemploss += cur_Dloss
             
-        cur_Gscore, _ = sess.run([Gscore, Gtrain_op], feed_dict={Zph:Z, Lph:labels, is_training:True})
-    
-        if it % disp_int == 0:
-            Gz_sample = sess.run(Gout_op, feed_dict={Zph: Z_test, Lph: labels_test, is_training:False})
-            vis_square(Gz_sample[:121], [11, 11], save_path=work_dir + 'Iter_%d.jpg' % it)
-            saver.save(sess, work_dir + "%.3d_model.ckpt" % it)
-            if ('cur_Dscore' in vars()) and ('cur_Gscore' in vars()):
-                print("Iteration #%4d, Train Gscore = %f, Dscore=%f" % (it, cur_Gscore, cur_Dscore))
+            if it % disp_int == 0:
+                Gsample = sess.run(Gout_op, feed_dict={Zph: Z, is_training:False})
+                vis_square(Gsample[:121], [11, 11], save_path=work_dir + 'Epoch%.3d.jpg' % data.train.epochs_completed)
+                saver.save(sess, work_dir + "%.3d_model.ckpt" % data.train.epochs_completed)
+                if ('cur_Dloss' in vars()) and ('cur_Gloss' in vars()):
+                    print("Epoch #%4d, Train Gloss = %f, Dloss=%f" % (data.train.epochs_completed, cur_Gloss, cur_Dloss))
+            
+        cur_Dloss = dtemploss / k
+            
+        Z = np.random.uniform(size=[batch_size, latentDim], low=-1., high=1.).astype(np.float32)
+        cur_Gloss, _ = sess.run([Gloss, Gtrain_op], feed_dict={Zph:Z, is_training:True})
