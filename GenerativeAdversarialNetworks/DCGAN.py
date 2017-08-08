b6f344230cdb1789eeca8e458ba8f98e23fa3a5d
Nima Ghorbani <nima.gbani@gmail.com>
2017-08-08 09:42:42 +0200

    improved dcgan

--- GenerativeAdversarialNetworks/DCGAN.py
+++ GenerativeAdversarialNetworks/DCGAN.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
-Generative Adversarial Networks - Goodfellow et al
-Unsupervised Representation Learning with Deep Convolutional Generative Adversarial Networks - Radford et al
+Generative Adversarial Networks - Goodfellow et al. 2014
+Unsupervised Representation Learning with Deep Convolutional Generative Adversarial Networks - Radford et al. 2015
 
 Use this code with no warranty and please respect the accompanying license.
 '''
@@ -20,28 +20,27 @@ from tools_networks import deconv, conv, dense, clipped_crossentropy, dropout
 from tensorflow.examples.tutorials.mnist import input_data
     
 def create_gan_G(z, is_training, Cout=1, trainable=True, reuse=False, networktype='ganG'):
-    '''input : batchsize * 100
-        output: batchsize * 28 * 28 * 1'''
+    '''input : batchsize * latentDim
+       output: batchsize * 28 * 28 * 1'''
     with tf.variable_scope(networktype, reuse=reuse):
-        Gout_op = dense(z, is_training, Cout=4 * 4 * 256, trainable=trainable, act='reLu', norm='batchnorm', name='dense1')
-        Gout_op = tf.reshape(Gout_op, shape=[-1, 4, 4, 256])  # 4
-        Gout_op = deconv(Gout_op, is_training, kernel_w=5, stride=2, Cout=256, trainable=trainable, act='reLu', norm='batchnorm', name='deconv1')  # 11
-        Gout_op = deconv(Gout_op, is_training, kernel_w=5, stride=2, Cout=128, trainable=trainable, act='reLu', norm='batchnorm', name='deconv2')  # 25
-        Gout_op = deconv(Gout_op, is_training, kernel_w=4, stride=1, Cout=Cout, trainable=trainable, act=None, norm=None, name='deconv3')  # 28
-        Gout_op = tf.nn.sigmoid(Gout_op)
-    return Gout_op
+        Gout = dense(z, is_training, Cout=7 * 7 * 256, trainable=trainable, act='reLu', norm='batchnorm', name='dense1')
+        Gout = tf.reshape(Gout, shape=[-1, 7, 7, 256])  # 7
+        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=128, trainable=trainable, act='reLu', norm='batchnorm', name='deconv1')  # 14
+        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=Cout, trainable=trainable, act=None, norm=None, name='deconv2')  # 28
+        Gout = tf.nn.sigmoid(Gout)
+    return Gout
 
 def create_gan_D(xz, is_training, trainable=True, reuse=False, networktype='ganD'):
     with tf.variable_scope(networktype, reuse=reuse):
-        Dxz = conv(xz, is_training, kernel_w=5, stride=2, Cout=128, trainable=trainable, act='lrelu', norm=None, name='conv1')  # 12
-        Dxz = conv(Dxz, is_training, kernel_w=5, stride=2, Cout=256, trainable=trainable, act='lrelu', norm='batchnorm', name='conv2')  # 4
-        Dxz = conv(Dxz, is_training, kernel_w=2, stride=2, Cout=256, trainable=trainable, act='lrelu', norm='batchnorm', name='conv3')  # 2
-        Dxz = conv(Dxz, is_training, kernel_w=2, stride=2, Cout=1, trainable=trainable, act=None, norm='batchnorm', name='conv4')  # 2
-        Dxz = tf.nn.sigmoid(Dxz)
-    return Dxz
+        Dout = conv(xz, is_training, kernel_w=4, stride=2, pad=1, Cout=128, trainable=trainable, act='lrelu', norm=None, name='conv1')  # 14
+        Dout = conv(Dout, is_training, kernel_w=4, stride=2, pad=1, Cout=256  , trainable=trainable, act='lrelu', norm='batchnorm', name='conv2')  # 7
+        Dout = conv(Dout, is_training, kernel_w=3, stride=1, pad=None, Cout=1, trainable=trainable, act=None, norm='batchnorm', name='conv4')  # 5
+        Dout = tf.nn.sigmoid(Dout)
+    return Dout
 
 def create_dcgan_trainer(base_lr=1e-4, networktype='dcgan', latentDim=100):
     '''Train a Generative Adversarial Network'''
+    eps = 1e-8
     is_training = tf.placeholder(tf.bool, [], 'is_training')
 
     Zph = tf.placeholder(tf.float32, [None, latentDim])  # tf.random_uniform(shape=[batch_size, 100], minval=-1., maxval=1., dtype=tf.float32)
@@ -61,6 +60,10 @@ def create_dcgan_trainer(base_lr=1e-4, networktype='dcgan', latentDim=100):
     Gloss = clipped_crossentropy(fakeLogits, tf.ones_like(fakeLogits))
     Dloss = clipped_crossentropy(fakeLogits, tf.zeros_like(fakeLogits)) + clipped_crossentropy(realLogits, tf.ones_like(realLogits))
     
+#     clipvals = lambda val: tf.clip_by_value(val, eps, 1. - eps)
+#     Dloss = tf.reduce_mean(tf.log(clipvals(realLogits)) + tf.log(1. - clipvals(fakeLogits)))
+#     Gloss = -tf.reduce_mean(tf.log(clipvals(fakeLogits)))
+    
     Gtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Gloss, var_list=G_varlist)
     Dtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Dloss, var_list=D_varlist)
     
@@ -71,14 +74,15 @@ if __name__ == '__main__':
     
     batch_size = 128
     base_lr = 2e-4
-    epochs = 1000
+    epochs = 500
     latentDim = 100
+    disp_every_epoch = 5
     
     work_dir = expr_dir + '%s/%s/' % (networktype, datetime.strftime(datetime.today(), '%Y%m%d'))
     if not os.path.exists(work_dir): os.makedirs(work_dir)
     
     data = input_data.read_data_sets(data_dir + '/' + networktype, reshape=False)
-    disp_int = 2 * int(data.train.num_examples / batch_size)  # every two epochs
+    disp_int = disp_every_epoch * int(data.train.num_examples / batch_size)  # every two epochs
     
     tf.reset_default_graph() 
     sess = tf.InteractiveSession()
@@ -87,7 +91,7 @@ if __name__ == '__main__':
     tf.global_variables_initializer().run()
     
     var_list = [var for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if (networktype.lower() in var.name.lower()) and ('adam' not in var.name.lower())]  
-    saver = tf.train.Saver(var_list=var_list, max_to_keep=1000)
+    saver = tf.train.Saver(var_list=var_list, max_to_keep=int(epochs * 0.1))
     # saver.restore(sess, expr_dir + 'ganMNIST/20170707/214_model.ckpt')  
         
     k = 1
