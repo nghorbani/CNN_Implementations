4e6b116f73dad517e8e72253665543f9d6b9e66b
Nima Ghorbani <nima.gbani@gmail.com>
2017-09-04 08:37:42 +0200

    Clean up and tiny bug fixes
    Trying to follow a uniform achitecture and naming convention between
    different architectures

--- GenerativeAdversarialNetworks/DCGAN.py
+++ GenerativeAdversarialNetworks/DCGAN.py
@@ -19,37 +19,37 @@ from tools_networks import deconv, conv, dense, clipped_crossentropy, dropout
 
 from tensorflow.examples.tutorials.mnist import input_data
     
-def create_gan_G(z, is_training, Cout=1, trainable=True, reuse=False, networktype='ganG'):
-    '''input : batchsize * latentDim
+def create_generator(Xin, is_training, Cout=1, reuse=False, networktype='ganG'):
+    '''input : batchsize * latentD
        output: batchsize * 28 * 28 * 1'''
     with tf.variable_scope(networktype, reuse=reuse):
-        Gout = dense(z, is_training, Cout=7 * 7 * 256, trainable=trainable, act='reLu', norm='batchnorm', name='dense1')
-        Gout = tf.reshape(Gout, shape=[-1, 7, 7, 256])  # 7
-        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=128, trainable=trainable, act='reLu', norm='batchnorm', name='deconv1')  # 14
-        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=Cout, trainable=trainable, act=None, norm=None, name='deconv2')  # 28
-        Gout = tf.nn.sigmoid(Gout)
-    return Gout
+        Xout = dense(Xin, is_training, Cout=7 * 7 * 256, act='reLu', norm='batchnorm', name='dense1')
+        Xout = tf.reshape(Xout, shape=[-1, 7, 7, 256])  # 7
+        Xout = deconv(Xout, is_training, kernel_w=4, stride=2, epf=2, Cout=128, act='reLu', norm='batchnorm', name='deconv1')  # 14
+        Xout = deconv(Xout, is_training, kernel_w=4, stride=2, epf=2, Cout=Cout, act=None, norm=None, name='deconv2')  # 28
+        Xout = tf.nn.sigmoid(Xout)
+    return Xout
 
-def create_gan_D(xz, is_training, trainable=True, reuse=False, networktype='ganD'):
+def create_Discriminator(Xin, is_training, reuse=False, networktype='ganD'):
     with tf.variable_scope(networktype, reuse=reuse):
-        Dout = conv(xz, is_training, kernel_w=4, stride=2, pad=1, Cout=128, trainable=trainable, act='lrelu', norm=None, name='conv1')  # 14
-        Dout = conv(Dout, is_training, kernel_w=4, stride=2, pad=1, Cout=256  , trainable=trainable, act='lrelu', norm='batchnorm', name='conv2')  # 7
-        Dout = conv(Dout, is_training, kernel_w=3, stride=1, pad=None, Cout=1, trainable=trainable, act=None, norm='batchnorm', name='conv4')  # 5
-        Dout = tf.nn.sigmoid(Dout)
-    return Dout
+        Xout = conv(Xin, is_training, kernel_w=4, stride=2, pad=1, Cout=128, act='lrelu', norm=None, name='conv1')  # 14
+        Xout = conv(Xout, is_training, kernel_w=4, stride=2, pad=1, Cout=256  , act='lrelu', norm='batchnorm', name='conv2')  # 7
+        Xout = conv(Xout, is_training, kernel_w=3, stride=1, pad=None, Cout=1, act=None, norm='batchnorm', name='conv4')  # 5
+        Xout = tf.nn.sigmoid(Xout)
+    return Xout
 
-def create_dcgan_trainer(base_lr=1e-4, networktype='dcgan', latentDim=100):
+def create_dcgan_trainer(base_lr=1e-4, latentD=100, networktype='dcgan'):
     '''Train a Generative Adversarial Network'''
     eps = 1e-8
     is_training = tf.placeholder(tf.bool, [], 'is_training')
 
-    Zph = tf.placeholder(tf.float32, [None, latentDim])  # tf.random_uniform(shape=[batch_size, 100], minval=-1., maxval=1., dtype=tf.float32)
+    Zph = tf.placeholder(tf.float32, [None, latentD])  # tf.random_uniform(shape=[batch_size, 100], minval=-1., maxval=1., dtype=tf.float32)
     Xph = tf.placeholder(tf.float32, [None, 28, 28, 1])
 
-    Gout_op = create_gan_G(Zph, is_training, Cout=1, trainable=True, reuse=False, networktype=networktype + '_G') 
+    Gout_op = create_generator(Zph, is_training, Cout=1, reuse=False, networktype=networktype + '_G') 
 
-    fakeLogits = create_gan_D(Gout_op, is_training, trainable=True, reuse=False, networktype=networktype + '_D')
-    realLogits = create_gan_D(Xph, is_training, trainable=True, reuse=True, networktype=networktype + '_D')
+    fakeLogits = create_Discriminator(Gout_op, is_training, reuse=False, networktype=networktype + '_D')
+    realLogits = create_Discriminator(Xph, is_training, reuse=True, networktype=networktype + '_D')
     
     G_varlist = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=networktype + '_G')
     print(len(G_varlist), [var.name for var in G_varlist])
@@ -59,10 +59,6 @@ def create_dcgan_trainer(base_lr=1e-4, networktype='dcgan', latentDim=100):
           
     Gloss = clipped_crossentropy(fakeLogits, tf.ones_like(fakeLogits))
     Dloss = clipped_crossentropy(fakeLogits, tf.zeros_like(fakeLogits)) + clipped_crossentropy(realLogits, tf.ones_like(realLogits))
-    
-#     clipvals = lambda val: tf.clip_by_value(val, eps, 1. - eps)
-#     Dloss = tf.reduce_mean(tf.log(clipvals(realLogits)) + tf.log(1. - clipvals(fakeLogits)))
-#     Gloss = -tf.reduce_mean(tf.log(clipvals(fakeLogits)))
     
     Gtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Gloss, var_list=G_varlist)
     Dtrain_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.5).minimize(Dloss, var_list=D_varlist)
@@ -75,7 +71,7 @@ if __name__ == '__main__':
     batch_size = 128
     base_lr = 2e-4
     epochs = 500
-    latentDim = 100
+    latentD = 2
     disp_every_epoch = 5
     
     work_dir = expr_dir + '%s/%s/' % (networktype, datetime.strftime(datetime.today(), '%Y%m%d'))
@@ -87,7 +83,7 @@ if __name__ == '__main__':
     tf.reset_default_graph() 
     sess = tf.InteractiveSession()
     
-    Gtrain_op, Dtrain_op, Gloss, Dloss, is_training, Zph, Xph, Gout_op = create_dcgan_trainer(base_lr, networktype=networktype)
+    Gtrain_op, Dtrain_op, Gloss, Dloss, is_training, Zph, Xph, Gout_op = create_dcgan_trainer(base_lr, latentD, networktype)
     tf.global_variables_initializer().run()
     
     var_list = [var for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if (networktype.lower() in var.name.lower()) and ('adam' not in var.name.lower())]  
@@ -103,7 +99,7 @@ if __name__ == '__main__':
         
         for itD in range(k):
             it += 1
-            Z = np.random.uniform(size=[batch_size, latentDim], low=-1., high=1.).astype(np.float32)
+            Z = np.random.uniform(size=[batch_size, latentD], low=-1., high=1.).astype(np.float32)
             X, _ = data.train.next_batch(batch_size)
             
             cur_Dloss, _ = sess.run([Dloss, Dtrain_op], feed_dict={Xph:X, Zph:Z, is_training:True})
@@ -113,7 +109,7 @@ if __name__ == '__main__':
              
         cur_Dloss = dtemploss / k   
         
-        Z = np.random.uniform(size=[batch_size, latentDim], low=-1., high=1.).astype(np.float32)     
+        Z = np.random.uniform(size=[batch_size, latentD], low=-1., high=1.).astype(np.float32)     
         cur_Gloss, _ = sess.run([Gloss, Gtrain_op], feed_dict={Zph:Z, is_training:True})
     
         if disp_losses:
--- VariationalAutoEncoders/VAE.py
+++ VariationalAutoEncoders/VAE.py
@@ -16,83 +16,75 @@ from datetime import datetime
 from tools_general import tf, np
 from tools_networks import deconv, conv, dense, clipped_crossentropy, dropout
   
-def create_VAE_E(Xin, is_training, latentW, latentC, reuse=False, networktype='vaeE'):
+def create_encoder(Xin, is_training, latentD, reuse=False, networktype='vaeE'):
     '''Xin: batchsize * H * W * Cin
        output1-2: batchsize * Cout'''
     with tf.variable_scope(networktype, reuse=reuse):
-        Eout = conv(Xin, is_training, kernel_w=4, stride=2, Cout=64, pad=1, act='reLu', norm='batchnorm', name='conv1')  # 14*14
-        Eout = conv(Eout, is_training, kernel_w=4, stride=2, Cout=128, pad=1, act='reLu', norm='batchnorm', name='conv2')  # 7*7
+        Xout = conv(Xin, is_training, kernel_w=4, stride=2, Cout=64, pad=1, act='reLu', norm='batchnorm', name='conv1')  # 14*14
+        Xout = conv(Xout, is_training, kernel_w=4, stride=2, Cout=128, pad=1, act='reLu', norm='batchnorm', name='conv2')  # 7*7
         
-        posteriorMu = conv(Eout, is_training, kernel_w=3, stride=1, Cout=latentC, pad=1, act=None, norm=None, name='conv_mu')
-        posteriorSigma = conv(Eout, is_training, kernel_w=3, stride=1, Cout=latentC, pad=1, act=None, norm=None, name='conv_sig')
-
-        posteriorMu = tf.reshape(posteriorMu, shape=[-1, latentW * latentW * latentC])
-        posteriorSigma = tf.reshape(posteriorSigma, shape=[-1, latentW * latentW * latentC])
+        posteriorMu_op = dense(Xout, is_training, Cout=latentD, act=None, norm=None, name='dense_mean')
+        posteriorSigma_op = dense(Xout, is_training, Cout=latentD, act=None, norm=None, name='dense_var')
         
-    return posteriorMu, posteriorSigma
+    return posteriorMu_op, posteriorSigma_op
 
-def create_VAE_D(z, is_training, Cout, latentW, latentC, reuse=False, networktype='vaeD'):
-    '''input : batchsize * latentDim
-       output: batchsize * 28 * 28 * 1'''
+def create_decoder(Xin, is_training, latentD, Cout=1, reuse=False, networktype='vaeE'):
     with tf.variable_scope(networktype, reuse=reuse):
-        print("Latent Space Dim = H=%d, W=%d, C=%d" % (latentW, latentW, latentC))
-        Gout = tf.reshape(z, shape=[-1, latentW, latentW, latentC])
-        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=128, act='reLu', norm='batchnorm', name='deconv1')  # 14
-        Gout = deconv(Gout, is_training, kernel_w=4, stride=2, epf=2, Cout=Cout, act=None, norm=None, name='deconv2')  # 28
-        Gout = tf.nn.sigmoid(Gout)
-    return Gout
-
-def create_vae_trainer(base_lr=1e-4, networktype='VAE', Cout=1, latentW=7, latentC=2):
+        Xout = dense(Xin, is_training, Cout=7 * 7 * 256, act='reLu', norm='batchnorm', name='dense1')
+        Xout = tf.reshape(Xout, shape=[-1, 7, 7, 256])  # 7
+        Xout = deconv(Xout, is_training, kernel_w=4, stride=2, Cout=256, epf=2, act='reLu', norm='batchnorm', name='deconv1')  # 14
+        Xout = deconv(Xout, is_training, kernel_w=4, stride=2, Cout=Cout, epf=2, act=None, norm=None, name='deconv2')  # 28
+        Xout = tf.nn.sigmoid(Xout)
+    return Xout
+    
+def create_vae_trainer(base_lr=1e-4, latentD=2, networktype='VAE'):
     '''Train a Variational AutoEncoder'''
     eps = 1e-5
     
     is_training = tf.placeholder(tf.bool, [], 'is_training')
 
-    Zph = tf.placeholder(tf.float32, [None, latentW * latentW * latentC])
+    Zph = tf.placeholder(tf.float32, [None, latentD])
     Xph = tf.placeholder(tf.float32, [None, 28, 28, 1])
 
-    posteriorMu, posteriorSigma = create_VAE_E(Xph, is_training, latentW, latentC, reuse=False, networktype=networktype + '_E') 
-    Z_op = posteriorSigma * Zph + posteriorMu
-    Xrec_op = create_VAE_D(Z_op, is_training, Cout, latentW, latentC, reuse=False, networktype=networktype + '_D')
+    posteriorMu_op, posteriorSigma_op = create_encoder(Xph, is_training, latentD, reuse=False, networktype=networktype + '_Enc') 
+    Z_op = posteriorSigma_op * Zph + posteriorMu_op
+    Xrec_op = create_decoder(Z_op, is_training, latentD, reuse=False, networktype=networktype + '_Dec')
     
     # E[log P(X|z)]
-    # rec_loss_op = tf.reduce_mean(tf.reduce_sum((Xph - 1.0) * tf.log(1.0 - Xrec_op + eps) - Xph * tf.log(Xrec_op + eps), reduction_indices=[1, 2, 3]))
     rec_loss_op = tf.reduce_mean(tf.reduce_sum(tf.square(tf.subtract(Xph, Xrec_op)), reduction_indices=[1, 2, 3]))
 
     # D_KL(Q(z|X) || P(z))
-    KL_loss = tf.reduce_mean(0.5 * tf.reduce_sum(tf.exp(posteriorSigma) + tf.square(posteriorMu) - 1 - posteriorSigma, reduction_indices=[1, ]))
-    
-    total_loss_op = tf.add(rec_loss_op , KL_loss)  
-    train_step_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.9).minimize(total_loss_op)
- 
-    E_varlist = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=networktype + '_E')    
-    D_varlist = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=networktype + '_D')
-    print('Total Trainable Variables Count in Encoder %2.3f M and in Decoder: %2.3f M.' % (count_model_params(E_varlist) / 1000000, count_model_params(D_varlist) / 1000000,))
+    KL_loss_op = tf.reduce_mean(0.5 * tf.reduce_sum(tf.exp(posteriorSigma_op) + tf.square(posteriorMu_op) - 1 - posteriorSigma_op, reduction_indices=[1, ]))
 
-    return train_step_op, total_loss_op, rec_loss_op, KL_loss, is_training, Zph, Xph, Xrec_op
+    Enc_varlist = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=networktype + '_Enc')    
+    Dec_varlist = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=networktype + '_Dec')
+    
+    total_loss_op = tf.add(rec_loss_op , KL_loss_op)  
+    train_step_op = tf.train.AdamOptimizer(learning_rate=base_lr, beta1=0.9).minimize(total_loss_op, var_list=Enc_varlist+Dec_varlist)
+ 
+    print('Total Trainable Variables Count in Encoder %2.3f M and in Decoder: %2.3f M.' % (count_model_params(Enc_varlist)*1e-6, count_model_params(Dec_varlist)*1e-6,))
+
+    return train_step_op, total_loss_op, rec_loss_op, KL_loss_op, is_training, Zph, Xph, Xrec_op
 
 if __name__ == '__main__':
     networktype = 'VAE_MNIST'
     
     batch_size = 128
+    
     base_lr = 1e-5
     epochs = 200
-    
-    Cout = 1
-    
-    latentW = 7
-    latentC = 2
-    latendDim = latentW * latentW * latentC
+        
+    latentD = 2
     
     work_dir = expr_dir + '%s/%s/' % (networktype, datetime.strftime(datetime.today(), '%Y%m%d'))
     if not os.path.exists(work_dir): os.makedirs(work_dir)
     
-    data, max_iter, test_iter, test_int, disp_int = get_train_params(data_dir + '/' + networktype, batch_size, epochs=epochs, test_in_each_epoch=1, networktype=networktype)
+    data, max_iter, test_iter, test_int, disp_int = get_train_params(data_dir, batch_size, epochs=epochs, test_in_each_epoch=1, networktype=networktype)
     
     tf.reset_default_graph() 
     sess = tf.InteractiveSession()
     
-    train_step_op, total_loss_op, rec_loss_op, KL_loss, is_training, Zph, Xph, Xrec_op = create_vae_trainer(base_lr, networktype, Cout, latentW, latentC)
+    train_step_op, total_loss_op, rec_loss_op, KL_loss_op, is_training, Zph, Xph, Xrec_op = create_vae_trainer(base_lr, latentD, networktype)
     tf.global_variables_initializer().run()
     
     var_list = [var for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if (networktype.lower() in var.name.lower()) and ('adam' not in var.name.lower())]  
@@ -105,13 +97,13 @@ if __name__ == '__main__':
     test_loss = np.zeros([int(np.ceil(max_iter / test_int)),3])
          
     for it in range(max_iter): 
-        Z = np.random.normal(size=[batch_size, latendDim], loc=0.0, scale=1.).astype(np.float32)
+        Z = np.random.normal(size=[batch_size, latentD], loc=0.0, scale=1.).astype(np.float32)
   
         if it % test_int == 0:  # Record summaries and test-set accuracy
             acc_loss = np.zeros([1,3])
             for i_test in range(test_iter):
                 X, _ = data.test.next_batch(batch_size)
-                resloss = sess.run([total_loss_op, rec_loss_op, KL_loss], feed_dict={Xph:X, Zph: Z, is_training:False})
+                resloss = sess.run([total_loss_op, rec_loss_op, KL_loss_op], feed_dict={Xph:X, Zph: Z, is_training:False})
                 acc_loss = np.add(acc_loss, resloss)
                  
             test_loss[it // test_int] = np.divide(acc_loss, test_iter)
